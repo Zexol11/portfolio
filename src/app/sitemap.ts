38433a7f93ea9f1e24f256cf440dd1c5fe970bdb
Ed Kenneth <ed.kenneth11@gmail.com>
2026-04-23 20:04:28 +0800

    fix: adjust type problem

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,7 +12,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   ].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
-    changeFrequency: (route === "" ? "daily" : "weekly") as const,
+    changeFrequency: route === "" ? ("daily" as const) : ("weekly" as const),
     priority: route === "" ? 1 : 0.8,
   }));
 
